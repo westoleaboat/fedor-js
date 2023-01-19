b1c9f6d443793103ace24ea264dbf0eaf053dc09
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-01-20 00:27:43 +0200

    load_dotenv() for environ var

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
 
     stores = [{"name": "My Store", "items": [
         {"name": "my item", "price": 15.99}]}]
