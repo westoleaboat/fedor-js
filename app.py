29f5f79b73a90c6885bb78a10af660dcfd76e10d
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-01-20 00:48:08 +0200

    added secret key

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask
+import os
 from dotenv import load_dotenv
 
 
 def create_app():
+
     app = Flask(__name__)
+
     load_dotenv()
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 
-    stores = [{"name": "My Store", "items": [
-        {"name": "my item", "price": 15.99}]}]
+    stores = [
+        {
+            "name": "My Store",
+            "items": [
+                {
+                    "name": "Chair",
+                    "price": 15.99
+                }
+            ]
+        }
+    ]
 
-    @app.get("/fedor")
+    @app.get("/")
     def get_stores():
         return {"stores": stores}
 
